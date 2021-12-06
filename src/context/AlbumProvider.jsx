e52393cefd8aded500f75e0e56d90f4bd20edea8
mariananbp <mariananbp@gmail.com>
2021-12-06 16:12:19 -0300

    Responsividade

--- src/context/AlbumProvider.jsx
+++ src/context/AlbumProvider.jsx
@@ -1,11 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import AlbumContext from './AlbumContext';
+import PropTypes from 'prop-types';
 
 function AlbumProvider({ children }) {
   const API_PHOTOS = 'https://api.pexels.com/v1/curated?per_page=10';
   const [photos, setPhotos] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [width, setWidth] = useState(0)
   const API_KEY = '563492ad6f9170000100000154ad4ac4ee4346c385b7fff974a8a2b6';
 
   useEffect(() => {
@@ -21,11 +23,14 @@ function AlbumProvider({ children }) {
       setLoading(false);
     }
     fetchAlbum();
+    const largura = window.innerWidth;
+    setWidth(largura);
   }, []);
 
   const contextValue = {
     photos,
     loading,
+    width,
   };
 
   return (
@@ -34,5 +39,9 @@ function AlbumProvider({ children }) {
     </AlbumContext.Provider>
   )
 }
+
+AlbumProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default AlbumProvider;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -3,22 +3,24 @@ import AlbumContext from '../context/AlbumContext';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import '../../node_modules/react-lazy-load-image-component/src/effects/blur.css';
 import '../styles/album.css';
+import Spinning from '../components/Spinning';
+
 
 function Album() {
-  const { photos, loading } = useContext(AlbumContext);
-  console.log(photos)
+  const { photos, loading, width } = useContext(AlbumContext);
+  console.log(width)
 
   return (
     <div className="album">
-      {loading ? 'loading...' : photos.map((photo, { scrollPosition }) => (
-        <div>
+      {loading ? <Spinning/> : photos.map((photo, { scrollPosition }) => (
+        <div className="photo">
           <LazyLoadImage
             effect="blur"
             src={photo.src.medium}
             alt={photo.photographer}
             key={photo.id}
-            height="400px"
-            width="300px"
+            height={width >= 320 ? "400px" : "250px"} // teve que ser tamanho físico por conta do lazyLoadImage
+            width={width >= 320 ? "300px" : "150px"}
             scrollPosition={scrollPosition}
             className="photos-album"
           />
