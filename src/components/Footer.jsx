c43c4777da1fc7ffebcf2b235a4d9d530e935ea6
mariananbp <mariananbp@gmail.com>
2021-12-05 21:12:57 -0300

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/album.css';
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <h4 className="author">Mariana Nogueira Brum Pandeló</h4>
+        <div className="nav">
+          <a href="https://www.linkedin.com/in/mariananogueirabrum/" className="links">Linkedin</a>
+          <a href="https://github.com/mariananogueirab" className="links">GitHub</a>
+        </div>
+      </div>
+      <a href="https://www.pexels.com">
+        <img src="https://images.pexels.com/lib/api/pexels-white.png" alt="logo pexels" className="logo-pexels"/>
+      </a>
+    </footer>
+  )
+}
+
+export default Footer;
