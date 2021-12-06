3a7ddd94b87e2f57e70268fdefdcdbd6bdedf037
mariananbp <mariananbp@gmail.com>
2021-12-05 21:07:22 -0300

    Provider

--- src/context/AlbumProvider.jsx
+++ src/context/AlbumProvider.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import AlbumContext from './AlbumContext';
 
 function AlbumProvider({ children }) {
-  let contextValue = {}
+  const API_PHOTOS = 'https://api.pexels.com/v1/curated?per_page=20';
+  const [photos, setPhotos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const API_KEY = '563492ad6f9170000100000154ad4ac4ee4346c385b7fff974a8a2b6';
+
+  useEffect(() => {
+    async function fetchAlbum() {
+      await axios.get(API_PHOTOS, { // autenticação necessária
+        headers: {
+          Authorization: `${API_KEY}`,
+        },
+      })
+        .then((data) => (
+          setPhotos(data.data.photos) // envia as fotos para o estado global
+        ));
+      setLoading(false);
+    }
+    fetchAlbum();
+  }, []);
+
+  const contextValue = {
+    photos,
+    loading,
+  };
+
   return (
     <AlbumContext.Provider value={ contextValue }>
       {children}
