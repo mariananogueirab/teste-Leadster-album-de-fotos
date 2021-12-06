a0ac93b9f7ad7dfac030a2974b03140013db6d72
mariananbp <mariananbp@gmail.com>
2021-12-05 21:13:40 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import AlbumProvider from './context/AlbumProvider';
 import './App.css';
 import Album from './pages/Album';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <AlbumProvider>
+        <Header />
         <Album />
+        <Footer />
       </AlbumProvider>
     </div>
   );
