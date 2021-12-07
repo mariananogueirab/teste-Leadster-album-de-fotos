05cf27083077a1b74e23e15284252d93e659c20f
mariananbp <mariananbp@gmail.com>
2021-12-06 22:50:37 -0300

    Icones no Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../styles/album.css';
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import AlbumContext from '../context/AlbumContext';
 
 function Footer() {
+  const { width } = useContext(AlbumContext);
+
   return (
     <footer>
       <div>
         <h4 className="author">Mariana Nogueira Brum Pandeló</h4>
         <div className="nav">
-          <a href="https://www.linkedin.com/in/mariananogueirabrum/" className="links">Linkedin</a>
-          <a href="https://github.com/mariananogueirab" className="links">GitHub</a>
+          <a href="https://www.linkedin.com/in/mariananogueirabrum/" className="links"><BsLinkedin size={width >= 320 ? "30px" : "10px"} /></a>
+          <a href="https://github.com/mariananogueirab" className="links"><BsGithub size={width >= 320 ? "30px" : "10px"} /></a>
         </div>
       </div>
       <a href="https://www.pexels.com">
