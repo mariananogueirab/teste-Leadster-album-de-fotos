a86c4758fd2a1c7d899ca3f9015c664c7d4dd270
mariananbp <mariananbp@gmail.com>
2021-12-08 16:50:51 -0300

    paginaçao dinamica

--- src/components/PaginationComponent.jsx
+++ src/components/PaginationComponent.jsx
@@ -4,8 +4,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import AlbumContext from '../context/AlbumContext';
 
 function PaginationComponent(){
-  const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const { setCurrentPage, currentPage } = useContext(AlbumContext);
+  const { setCurrentPage, currentPage, totalResults, perPage } = useContext(AlbumContext);
+  const totalPages = Math.ceil(totalResults/perPage);
+  let pages = [];
+  let pagesForShow = [];
+
+  for (let page = 1; page <= totalPages; page ++) {
+    pages.push(page);
+  }
+
+  for (let page = currentPage; page <= (currentPage +2); page ++) {
+    if(currentPage !== totalPages) {
+      pagesForShow.push(page);
+    }
+  } // mostra apenas 3 páginas na paginação
+
+  for (let page = (currentPage - 2); page <= totalPages; page ++) {
+    if(currentPage === totalPages) {
+      pagesForShow.push(page);
+    }
+  } // para mostrar a última página e duas anteriores
 
   const firstPage = () => { // volta pra primeira página
     setCurrentPage(1);
@@ -34,14 +52,16 @@ function PaginationComponent(){
   
   return (
     <div className="pagination">
-      <Pagination  size="lg">
+      <Pagination  size="lg" pageNeighbours={1}>
         <Pagination.First onClick={firstPage} />
         <Pagination.Prev onClick={prevPage} />
-          {pages.map((page, index) => (
-        <Pagination.Item key={page} active={page === currentPage} index={index} onClick={() => currPage(page)}>
+        <Pagination.Ellipsis />
+          {pagesForShow.map((page, index) => (
+            <Pagination.Item key={page} active={page === currentPage} index={index} onClick={() => currPage(page)}>
          {page}
         </Pagination.Item>
       ))}
+        <Pagination.Ellipsis />
         <Pagination.Next onClick={nextPage}/>
         <Pagination.Last onClick={lastPage}/>
       </Pagination>
--- src/context/AlbumProvider.jsx
+++ src/context/AlbumProvider.jsx
@@ -4,12 +4,14 @@ import AlbumContext from './AlbumContext';
 import PropTypes from 'prop-types';
 
 function AlbumProvider({ children }) {
+  const perPage = 80;
   const [photos, setPhotos] = useState([]);
   const [loading, setLoading] = useState(true);
   const [width, setWidth] = useState(0);
   const [searchText, setSearchText] = useState('nature');
   const [currentPage, setCurrentPage] = useState(1);
-  const API_PHOTOS = `https://api.pexels.com/v1/search/?page=${currentPage}&per_page=20&query=${searchText === '' ? 'nature' : searchText}`;
+  const [totalResults, setTotalResults] = useState(0);
+  const API_PHOTOS = `https://api.pexels.com/v1/search/?page=${currentPage}&per_page=${perPage}&query=${searchText === '' ? 'nature' : searchText}`;
   const API_KEY = '563492ad6f9170000100000154ad4ac4ee4346c385b7fff974a8a2b6';
 
   useEffect(() => {
@@ -19,9 +21,10 @@ function AlbumProvider({ children }) {
           Authorization: `${API_KEY}`,
         },
       })
-        .then((data) => (
-          setPhotos(data.data.photos) // envia as fotos para o estado global
-        ));
+        .then((data) => {
+          setPhotos(data.data.photos); // envia as fotos para o estado global
+          setTotalResults(data.data.total_results)
+        });
       setLoading(false);
     }
     fetchAlbum();
@@ -36,7 +39,9 @@ function AlbumProvider({ children }) {
     setSearchText,
     searchText,
     setCurrentPage,
-    currentPage
+    currentPage,
+    totalResults,
+    perPage
   };
 
   return (
