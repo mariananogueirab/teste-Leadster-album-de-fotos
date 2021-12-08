9e681507847c064ad9505d766d5e7332cb66f88e
mariananbp <mariananbp@gmail.com>
2021-12-08 15:10:08 -0300

    album

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -5,29 +5,33 @@ import '../../node_modules/react-lazy-load-image-component/src/effects/blur.css'
 import '../styles/album.css';
 import Spinning from '../components/Spinning';
 import CollectionNotFound from '../components/CollectionNotFound';
+import PaginationComponent from '../components/PaginationComponent';
 
 
 function Album() {
   const { photos, loading, width, searchText } = useContext(AlbumContext);
 
   return (
-    <div className="album">
-      {searchText.lengt > 0 && photos.length === 0 && <CollectionNotFound/>}
-      {loading ? <Spinning/> : photos.map((photo, { scrollPosition }) => (
-        <div className="photo">
-          <LazyLoadImage
-            effect="blur"
-            src={photo.src.medium}
-            alt={photo.photographer}
-            key={photo.id}
-            height={width >= 320 ? "400px" : "250px"} // teve que ser tamanho físico por conta do lazyLoadImage
-            width={width >= 320 ? "300px" : "150px"}
-            scrollPosition={scrollPosition}
-            className="photos-album"
-          />
-        </div>
-      ))}
-    </div>
+    <main>
+      {searchText.length > 0 && photos.length === 0 && <CollectionNotFound/>}
+      <section className="album">
+        {loading ? <Spinning/> : photos.map((photo, { scrollPosition }) => (
+          <div className="photo">
+            <LazyLoadImage
+              effect="blur"
+              src={photo.src.medium}
+              alt={photo.photographer}
+              key={photo.id}
+              height={width >= 320 ? "400px" : "300px"} // teve que ser tamanho físico por conta do lazyLoadImage
+              width={width >= 320 ? "300px" : "150px"}
+              scrollPosition={scrollPosition}
+              className="photos-album"
+            />
+          </div>
+        ))}
+      </section>
+      <PaginationComponent />
+    </main>
   )
 }
 
