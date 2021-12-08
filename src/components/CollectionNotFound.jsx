3248e61057ec89496547b239dfdb3fd6053e2b84
mariananbp <mariananbp@gmail.com>
2021-12-08 14:53:52 -0300

    Collection not found

--- src/components/CollectionNotFound.jsx
+++ src/components/CollectionNotFound.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BiSad } from "react-icons/bi";
+import AlbumContext from '../context/AlbumContext';
 import '../styles/album.css';
 
 function CollectionNotFound() {
+  const { width } = useContext(AlbumContext);
+
   return (
     <div className="not-found">
       Desculpe, não encontramos uma coleção com esse nome
-      <BiSad />
+      <BiSad size={width >= 320 ? "50px" : "10px"}/>
     </div>
   );
 };
