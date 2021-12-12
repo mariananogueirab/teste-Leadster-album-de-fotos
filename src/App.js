541a663f56847b1c07f579c828da1a3e1002ff37
mariananbp <mariananbp@gmail.com>
2021-12-12 18:12:16 -0300

    sass

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Album from './pages/Album';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import SearchBar from './components/SearchBar';
-import './styles/album.css';
 
 function App() {
   return (
--- src/components/Spinning.jsx
+++ src/components/Spinning.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Spinner from 'react-bootstrap/Spinner';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Spinning = function SpinnerFunc() {
   return (
